d90b85e685669906ce633f94b867a09b13157914
Yousif <lunatic.9205@icloud.com>
2026-09-09 10:56:43 +0300

    sets all of models and Schema's plus improt them to the server

---
+++ models/listing.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const listingSchema = new mongoose.Schema({
+    streetAddress:{
+        type:String,
+        required:true
+    },
+    city:{
+        type:String,
+        required:true
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    size:{
+        type:Number
+    },
+    owner:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+    category:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Category',
+        required:true
+    },
+
+}, { timestamps: true})
+
+const listing = mongoose.model('Listing',listingSchemaSchema)
+module.exports = Listing
---
+++ models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    userName:{
+        type:String,
+        required:true,
+        unique:true
+    },
+     passWord:{
+            type:String,
+            required:true
+        },
+
+}, {timestamps: true})
+
+
+const User = mongoose.model('User',userSchema)
+module.exports = User
